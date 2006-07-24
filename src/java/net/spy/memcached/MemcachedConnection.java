03f353e39d23c71825405acd22db94428323997b
Dustin Sallings <dustin@spy.net>
2006-07-24 15:03:08 -0700

    Deal with memcached restarting.

--- src/java/net/spy/memcached/MemcachedConnection.java
+++ src/java/net/spy/memcached/MemcachedConnection.java
@@ -30,9 +30,10 @@ public class MemcachedConnection extends SpyObject {
 		for(SocketAddress sa : a) {
 			SocketChannel ch=SocketChannel.open(sa);
 			ch.configureBlocking(false);
-			QueueAttachment qa=new QueueAttachment(ch);
+			QueueAttachment qa=new QueueAttachment(sa, ch);
 			connections[cons]=ch.register(selector, 0, qa);
 			qa.sk=connections[cons];
+			qa.which=cons;
 			cons++;
 		}
 	}
@@ -46,63 +47,105 @@ public class MemcachedConnection extends SpyObject {
 			for(SelectionKey sk : selectedKeys) {
 				getLogger().debug("Got selection key:  %s (r=%s, w=%s)",
 						sk, sk.isReadable(), sk.isWritable());
-				QueueAttachment qa=(QueueAttachment)sk.attachment();
-				if(qa.ops.size() > 0) {
-					Operation currentOp=qa.ops.peek();
-					getLogger().debug("Current operation: %s", currentOp);
-					// First switch is for IO.
-					switch(currentOp.getState()) {
-						case READING:
-							if(sk.isReadable()) {
-								qa.channel.read(qa.buf);
-								qa.buf.flip();
-								currentOp.readFromBuffer(qa.buf);
-								qa.buf.clear();
-							}
-							break;
-						case WRITING:
-							if(sk.isWritable()) {
-								ByteBuffer b=currentOp.getBuffer();
-								int written=qa.channel.write(b);
-								getLogger().debug("Wrote %d bytes for %s",
-										written, currentOp);
-								if(b.remaining() == 0) {
-									currentOp.writeComplete();
-								}
-							}
-							break;
-						case COMPLETE:
-							assert false : "Current op is in complete state";
-						break;
-					}
-					// Second switch is for post-IO examination and state transition
-					switch(currentOp.getState()) {
-						case READING:
-							sk.interestOps(SelectionKey.OP_READ);
-							break;
-						case WRITING:
-							getLogger().debug("Operation is still writing.");
-							sk.interestOps(SelectionKey.OP_WRITE);
-							break;
-						case COMPLETE:
-							qa.ops.remove();
-							// If there are more operations in the queue, tell
-							// it we want to write
-							synchronized(qa) {
-								if(qa.ops.size() > 0) {
-									sk.interestOps(SelectionKey.OP_WRITE);
-								}
-							}
-							break;
-					}
-				} else {
-					assert false : "No current operations, but selectors ready";
-				}
+				handleIO(sk);
 			} // for each selector
 			selectedKeys.clear();
 		} else {
 			getLogger().info("No selectors ready, interrupted: "
 				+ Thread.interrupted());
+		}
+	}
+
+	// Handle IO for a specific selector.
+	private void handleIO(SelectionKey sk) throws IOException {
+		QueueAttachment qa=(QueueAttachment)sk.attachment();
+		if(qa.ops.size() > 0) {
+			Operation currentOp=qa.ops.peek();
+			getLogger().debug("Current operation: %s", currentOp);
+			// First switch is for IO.
+			switch(currentOp.getState()) {
+				case READING:
+					if(sk.isReadable()) {
+						qa.channel.read(qa.buf);
+						qa.buf.flip();
+						currentOp.readFromBuffer(qa.buf);
+						qa.buf.clear();
+					}
+					break;
+				case WRITING:
+					if(sk.isWritable()) {
+						ByteBuffer b=currentOp.getBuffer();
+						int written=qa.channel.write(b);
+						getLogger().debug("Wrote %d bytes for %s",
+								written, currentOp);
+						if(b.remaining() == 0) {
+							currentOp.writeComplete();
+						}
+					}
+					break;
+				case COMPLETE:
+					assert false : "Current op is in complete state";
+				break;
+			}
+			// Second switch is for post-IO examination and state transition
+			switch(currentOp.getState()) {
+				case READING:
+					sk.interestOps(SelectionKey.OP_READ);
+					break;
+				case WRITING:
+					getLogger().debug("Operation is still writing.");
+					sk.interestOps(SelectionKey.OP_WRITE);
+					break;
+				case COMPLETE:
+					qa.ops.remove();
+					// If there are more operations in the queue, tell
+					// it we want to write
+					synchronized(qa) {
+						if(qa.ops.size() > 0) {
+							sk.interestOps(SelectionKey.OP_WRITE);
+						}
+					}
+					break;
+			}
+		} else {
+			if(sk.isConnectable()) {
+				getLogger().warn("Connection state changed for " + sk);
+				try {
+					if(qa.channel.finishConnect()) {
+						synchronized(qa) {
+							if(qa.ops.size() > 0) {
+								sk.interestOps(SelectionKey.OP_WRITE);
+							} else {
+								sk.interestOps(0);
+							}
+						}
+					}
+				} catch(IOException e) {
+					reconnect(qa);
+				}
+			} else if(sk.isReadable()) {
+				ByteBuffer b=ByteBuffer.allocate(1);
+				int read=qa.channel.read(b);
+				assert read == -1 : "expected to read -1 bytes, read " + read;
+				reconnect(qa);
+			} else {
+				assert false : "No current operations, but selectors ready";
+			}
+		}
+	}
+
+	private void reconnect(QueueAttachment qa) throws IOException {
+		getLogger().warn("Closing, and reopening connection.");
+		synchronized(qa) {
+			qa.sk.cancel();
+			SocketChannel ch=SocketChannel.open();
+			ch.configureBlocking(false);
+			ch.connect(qa.socketAddress);
+			qa.channel.socket().close();
+			qa.channel=ch;
+			connections[qa.which]=ch.register(selector, 0, qa);
+			qa.sk=connections[qa.which];
+			qa.sk.interestOps(SelectionKey.OP_CONNECT);
 		}
 	}
 
@@ -121,7 +164,7 @@ public class MemcachedConnection extends SpyObject {
 	 */
 	public SocketAddress getAddressOf(int which) {
 		QueueAttachment qa=(QueueAttachment)connections[which].attachment();
-		return qa.channel.socket().getRemoteSocketAddress();
+		return qa.socketAddress;
 	}
 
 	/**
@@ -161,12 +204,15 @@ public class MemcachedConnection extends SpyObject {
 	}
 
 	private static class QueueAttachment {
+		public int which=0;
+		public SocketAddress socketAddress=null;
 		public SocketChannel channel=null;
 		public ByteBuffer buf=null;
 		public LinkedBlockingQueue<Operation> ops=null;
 		public SelectionKey sk=null;
-		public QueueAttachment(SocketChannel c) {
+		public QueueAttachment(SocketAddress sa, SocketChannel c) {
 			super();
+			socketAddress=sa;
 			channel=c;
 			buf=ByteBuffer.allocate(4096);
 			ops=new LinkedBlockingQueue<Operation>();
