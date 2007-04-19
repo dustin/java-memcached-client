4acee0638643ba0880bde11cb061a32734f25b4f
Dustin Sallings <dustin@spy.net>
2007-04-18 23:34:38 -0700

    Use a bigger buffer size for the ``lots of sets'' test.

--- src/test/net/spy/memcached/DoLotsOfSets.java
+++ src/test/net/spy/memcached/DoLotsOfSets.java
@@ -12,7 +12,7 @@ public class DoLotsOfSets {
 		// Create a client with a queue big enough to hold the 300,000 items
 		// we're going to add.
 		MemcachedClient client=new MemcachedClient(
-			new DefaultConnectionFactory(350000, 8192),
+			new DefaultConnectionFactory(350000, 32768),
 			AddrUtil.getAddresses("localhost:11211"));
 		long start=System.currentTimeMillis();
 		byte[] toStore=new byte[26];
