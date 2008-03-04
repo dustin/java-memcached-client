2f6a4f5d813fdbba7c8ca34cdd5cfbd539bf7b4e
Blair Zajac <blair@orcaware.com>
2008-03-04 15:50:13 -0800

    Add a new transcoder for Long's.

---
+++ src/main/java/net/spy/memcached/transcoders/LongTranscoder.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2006  Dustin Sallings <dustin@spy.net>
+
+package net.spy.memcached.transcoders;
+
+import java.io.ObjectStreamClass;
+
+import net.spy.SpyObject;
+import net.spy.util.CloseUtil;
+
+import net.spy.memcached.CachedData;
+
+/**
+ * Transcoder that serializes and unserializes longs.
+ */
+public final class LongTranscoder extends SpyObject
+    implements Transcoder<Long> {
+
+    private static final int flags = TranscoderUtils.hashForFlags(ObjectStreamClass.lookup(Long.class).getSerialVersionUID());
+
+    public CachedData encode(java.lang.Long l) {
+        return new CachedData(flags, TranscoderUtils.encodeLong(l));
+    }
+
+    public Long decode(CachedData d) {
+        if (flags == d.getFlags()) {
+            return TranscoderUtils.decodeLong(d.getData());
+        } else {
+            return null;
+        }
+    }
+
+}
