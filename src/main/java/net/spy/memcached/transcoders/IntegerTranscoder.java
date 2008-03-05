66bca07ecb4cc368cf817bd7734c4e63cd8a553b
Blair Zajac <blair@orcaware.com>
2008-03-04 17:11:32 -0800

    New Transcoder for java.lang.Integer's.

---
+++ src/main/java/net/spy/memcached/transcoders/IntegerTranscoder.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2006  Dustin Sallings <dustin@spy.net>
+
+package net.spy.memcached.transcoders;
+
+import java.io.ObjectStreamClass;
+
+import net.spy.SpyObject;
+import net.spy.util.CloseUtil;
+
+import net.spy.memcached.CachedData;
+
+/**
+ * Transcoder that serializes and unserializes longs.
+ */
+public final class IntegerTranscoder extends SpyObject
+	implements Transcoder<Integer> {
+
+	private static final int flags = TranscoderUtils.hashForFlags(ObjectStreamClass.lookup(Integer.class).getSerialVersionUID());
+
+	public CachedData encode(java.lang.Integer l) {
+		return new CachedData(flags, TranscoderUtils.encodeInt(l));
+	}
+
+	public Integer decode(CachedData d) {
+		if (flags == d.getFlags()) {
+			return TranscoderUtils.decodeInt(d.getData());
+		} else {
+			return null;
+		}
+	}
+
+}
