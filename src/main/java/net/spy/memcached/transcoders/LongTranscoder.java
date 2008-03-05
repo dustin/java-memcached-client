3636c12337efa2041c2489121d3f03ab2c262671
Blair Zajac <blair@orcaware.com>
2008-03-04 17:08:35 -0800

    Change four spaces to a single tab to be consitent with all the other code.

--- src/main/java/net/spy/memcached/transcoders/LongTranscoder.java
+++ src/main/java/net/spy/memcached/transcoders/LongTranscoder.java
@@ -13,20 +13,20 @@ import net.spy.memcached.CachedData;
  * Transcoder that serializes and unserializes longs.
  */
 public final class LongTranscoder extends SpyObject
-    implements Transcoder<Long> {
+	implements Transcoder<Long> {
 
-    private static final int flags = TranscoderUtils.hashForFlags(ObjectStreamClass.lookup(Long.class).getSerialVersionUID());
+	private static final int flags = TranscoderUtils.hashForFlags(ObjectStreamClass.lookup(Long.class).getSerialVersionUID());
 
-    public CachedData encode(java.lang.Long l) {
-        return new CachedData(flags, TranscoderUtils.encodeLong(l));
-    }
+	public CachedData encode(java.lang.Long l) {
+		return new CachedData(flags, TranscoderUtils.encodeLong(l));
+	}
 
-    public Long decode(CachedData d) {
-        if (flags == d.getFlags()) {
-            return TranscoderUtils.decodeLong(d.getData());
-        } else {
-            return null;
-        }
-    }
+	public Long decode(CachedData d) {
+		if (flags == d.getFlags()) {
+			return TranscoderUtils.decodeLong(d.getData());
+		} else {
+			return null;
+		}
+	}
 
 }
