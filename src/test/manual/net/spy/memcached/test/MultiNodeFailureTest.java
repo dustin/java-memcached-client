8d4dd42a36a750490d6ec49289e6f360ea3a475e
Dustin Sallings <dustin@spy.net>
2008-03-13 23:42:11 -0700

    Added a manual test to try to reproduce a problem.

---
+++ src/test/manual/net/spy/memcached/test/MultiNodeFailureTest.java
@@ -0,0 +1,27 @@
+package net.spy.memcached.test;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.MemcachedClient;
+
+/**
+ * This is an attempt to reproduce a problem where a server fails during a
+ * series of gets.
+ */
+public class MultiNodeFailureTest {
+
+	public static void main(String args[]) throws Exception {
+		MemcachedClient c=new MemcachedClient(
+			AddrUtil.getAddresses("localhost:11200 localhost:11201"));
+		while(true) {
+			for(int i=0; i<1000; i++) {
+				try {
+					c.getBulk("blah1", "blah2", "blah3", "blah4", "blah5");
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Did a thousand.");
+		}
+	}
+
+}
