57379c949b482f547bb0ace8629f1c4ee03706fa
Dustin Sallings <dustin@spy.net>
2008-05-02 20:21:41 -0700

    Tests for timeout operations.

---
+++ src/test/java/net/spy/memcached/TimeoutTests.java
@@ -0,0 +1,66 @@
+package net.spy.memcached;
+
+public class TimeoutTests extends ClientBaseCase {
+
+	@Override
+	protected void tearDown() throws Exception {
+		// override teardown to avoid the flush phase
+		client.shutdown();
+	}
+
+	@Override
+	protected void initClient() throws Exception {
+		client=new MemcachedClient(new DefaultConnectionFactory() {
+			@Override
+			public long getOperationTimeout() {
+				return 1;
+			}},
+			AddrUtil.getAddresses("127.0.0.1:11213"));
+	}
+
+	private void tryTimeout(String name, Runnable r) {
+		try {
+			r.run();
+			fail("Expected timeout in " + name);
+		} catch(OperationTimeoutException e) {
+			// pass
+		}
+	}
+
+	public void testCasTimeout() {
+		tryTimeout("cas", new Runnable() {public void run() {
+			client.cas("k", 1, "blah");
+		}});
+	}
+
+	public void testGetsTimeout() {
+		tryTimeout("gets", new Runnable() {public void run() {
+			client.gets("k");
+		}});
+	}
+
+	public void testGetTimeout() {
+		tryTimeout("get", new Runnable() {public void run() {
+			client.get("k");
+		}});
+	}
+
+	public void testGetBulkTimeout() {
+		tryTimeout("getbulk", new Runnable() {public void run() {
+			client.getBulk("k", "k2");
+		}});
+	}
+
+	public void testIncrTimeout() {
+		tryTimeout("incr", new Runnable() {public void run() {
+			client.incr("k", 1);
+		}});
+	}
+
+	public void testIncrWithDefTimeout() {
+		tryTimeout("incrWithDef", new Runnable() {public void run() {
+			client.incr("k", 1, 5);
+		}});
+	}
+
+}
