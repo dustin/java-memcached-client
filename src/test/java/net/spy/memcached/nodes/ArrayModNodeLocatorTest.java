af61afb462952544a3f6e8d6aa4af094f5b9fc20
Dustin Sallings <dustin@spy.net>
2008-05-04 16:45:08 -0700

    Swapped out NATIVE_HASH in favor of FNV1A_32_HASH for arraymod locator test.

--- src/test/java/net/spy/memcached/nodes/ArrayModNodeLocatorTest.java
+++ src/test/java/net/spy/memcached/nodes/ArrayModNodeLocatorTest.java
@@ -3,10 +3,6 @@ package net.spy.memcached.nodes;
 import java.util.Arrays;
 import java.util.Collection;
 
-import net.spy.memcached.nodes.ArrayModNodeLocator;
-import net.spy.memcached.nodes.HashAlgorithm;
-import net.spy.memcached.nodes.MemcachedNode;
-
 /**
  * Test the ArrayModNodeLocator.
  */
@@ -16,23 +12,23 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 	protected void setupNodes(int n) {
 		super.setupNodes(n);
 		locator=new ArrayModNodeLocator(Arrays.asList(nodes),
-			HashAlgorithm.NATIVE_HASH);
+			HashAlgorithm.FNV1A_32_HASH);
 	}
 
 	public void testPrimary() throws Exception {
 		setupNodes(4);
-		assertSame(nodes[3], locator.getPrimary("dustin"));
-		assertSame(nodes[0], locator.getPrimary("x"));
-		assertSame(nodes[1], locator.getPrimary("y"));
+		assertSame(nodes[2], locator.getPrimary("dustin"));
+		assertSame(nodes[3], locator.getPrimary("x"));
+		assertSame(nodes[0], locator.getPrimary("y"));
 	}
 
 	public void testPrimaryClone() throws Exception {
 		setupNodes(4);
-		assertEquals(nodes[3].toString(),
+		assertEquals(nodes[2].toString(),
 			locator.getReadonlyCopy().getPrimary("dustin").toString());
-		assertEquals(nodes[0].toString(),
+		assertEquals(nodes[3].toString(),
 			locator.getReadonlyCopy().getPrimary("x").toString());
-		assertEquals(nodes[1].toString(),
+		assertEquals(nodes[0].toString(),
 			locator.getReadonlyCopy().getPrimary("y").toString());
 	}
 
@@ -54,12 +50,12 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 
 	public void testSeq1() {
 		setupNodes(4);
-		assertSequence("dustin", 0, 1, 2);
+		assertSequence("dustin", 3, 0, 1);
 	}
 
 	public void testSeq2() {
 		setupNodes(4);
-		assertSequence("noelani", 1, 2, 3);
+		assertSequence("noelani", 0, 1, 2);
 	}
 
 	public void testSeqOnlyOneServer() {
@@ -69,6 +65,6 @@ public class ArrayModNodeLocatorTest extends AbstractNodeLocationCase {
 
 	public void testSeqWithTwoNodes() {
 		setupNodes(2);
-		assertSequence("dustin", 0);
+		assertSequence("dustin", 1);
 	}
 }
