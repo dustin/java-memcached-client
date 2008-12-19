fe634da3901afa21955c99d667557c72cb540b1f
Dustin Sallings <dustin@spy.net>
2008-12-19 09:26:03 -0800

    A null key -> bytes should return null.

--- src/main/java/net/spy/memcached/util/KeyUtil.java
+++ src/main/java/net/spy/memcached/util/KeyUtil.java
@@ -17,7 +17,7 @@ public class KeyUtil {
 	 */
 	public static byte[] getKeyBytes(String k) {
 		try {
-			return k.getBytes("UTF-8");
+			return k == null ? null : k.getBytes("UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
