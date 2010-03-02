cba26c186143f8adcbc7f8fbcb8d2dd2fbf9df91
Dustin Sallings <dustin@spy.net>
2010-03-02 01:36:09 -0800

    If the String value of the socket address starts with a /, remove it.
    This is just the default stringification, clients can use whatever
    they want, but by default, this seems to provide consistent results
    with libketama.

--- src/main/java/net/spy/memcached/util/DefaultKetamaNodeLocatorConfiguration.java
+++ src/main/java/net/spy/memcached/util/DefaultKetamaNodeLocatorConfiguration.java
@@ -37,6 +37,9 @@ public class DefaultKetamaNodeLocatorConfiguration implements
 		String result=socketAddresses.get(node);
 		if(result == null) {
 			result=String.valueOf(node.getSocketAddress());
+			if (result.startsWith("/")) {
+				result = result.substring(1);
+			}
 			socketAddresses.put(node, result);
 		}
 		return result;
