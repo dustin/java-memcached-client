7ceca517bac2a1295da1b7fa8626695c2216fbb5
Blair Zajac <blair@orcaware.com>
2010-06-03 13:10:30 -0700

    No need to call MessageDigest#reset() on a brand new MessageDigest.
    Change-Id: I214f4fe0edb07c821f139b61526a2547d9772324
    Reviewed-on: http://review.northscale.com:8080/290
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- src/main/java/net/spy/memcached/HashAlgorithm.java
+++ src/main/java/net/spy/memcached/HashAlgorithm.java
@@ -131,7 +131,6 @@ public enum HashAlgorithm {
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException("MD5 not supported", e);
 		}
-		md5.reset();
 		md5.update(KeyUtil.getKeyBytes(k));
 		return md5.digest();
 	}
