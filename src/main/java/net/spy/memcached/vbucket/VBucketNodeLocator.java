10ece6929593a30e34c6bd47353f2112f000c5f4
Matt Ingenthron <ingenthr@cep.net>
2011-03-18 12:53:01 -0700

    Added logging for manipulation of nodesMap in VBucketLocator.
    Some situations have come up which are unexepctedly causing the
    nodesMap to return a null when identifying the primary for a given
    key.  We will now debug log how we build that map and look at the
    insides of the nodesMap if this situation occurs.
    
    Change-Id: Ia70079265863f17acdb93cc0115e8d43a8930e13
    Signed-off-by: Matt Ingenthron <ingenthr@cep.net>
    Reviewed-on: http://review.couchbase.org/6866
    Tested-by: Matt Ingenthron <matt@couchbase.com>
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>

--- src/main/java/net/spy/memcached/vbucket/VBucketNodeLocator.java
+++ src/main/java/net/spy/memcached/vbucket/VBucketNodeLocator.java
@@ -2,6 +2,7 @@ package net.spy.memcached.vbucket;
 
 import net.spy.memcached.MemcachedNode;
 import net.spy.memcached.NodeLocator;
+import net.spy.memcached.compat.SpyObject;
 import net.spy.memcached.vbucket.config.Config;
 
 import java.util.Collection;
@@ -14,7 +15,7 @@ import java.net.InetSocketAddress;
 /**
  * Implementation of the {@link NodeLocator} interface that contains vbucket hashing methods
  */
-public class VBucketNodeLocator implements NodeLocator {
+public class VBucketNodeLocator extends SpyObject implements NodeLocator {
 
     private Map<String, MemcachedNode> nodesMap;
 
@@ -41,6 +42,14 @@ public class VBucketNodeLocator implements NodeLocator {
         String server = config.getServer(serverNumber);
         // choose appropriate MemecachedNode according to config data
         MemcachedNode pNode = nodesMap.get(server);
+        if (pNode == null) {
+            getLogger().error("The node locator does not have a primary for key %s.", k);
+            Collection<MemcachedNode> nodes = nodesMap.values();
+            getLogger().error("MemcachedNode has %s entries:", nodesMap.size());
+            for (MemcachedNode node : nodes) {
+                getLogger().error(node);
+            }
+        }
         assert (pNode != null);
         return pNode;
     }
@@ -81,10 +90,13 @@ public class VBucketNodeLocator implements NodeLocator {
 
     private void setNodes(Collection<MemcachedNode> nodes) {
         HashMap<String, MemcachedNode> vbnodesMap = new HashMap<String, MemcachedNode>();
+        getLogger().debug("Updating nodesMap in VBucketNodeLocator.");
         for (MemcachedNode node : nodes) {
             InetSocketAddress addr = (InetSocketAddress) node.getSocketAddress();
             String address = addr.getAddress().getHostName() + ":" + addr.getPort();
 	    String hostname = addr.getAddress().getHostAddress() + ":" + addr.getPort();
+	    getLogger().debug("Adding node with hostname %s and address %s.", hostname, address);
+	    getLogger().debug("Node added is %s.", node);
             vbnodesMap.put(address, node);
 	    vbnodesMap.put(hostname, node);
         }
@@ -100,7 +112,7 @@ public class VBucketNodeLocator implements NodeLocator {
      * Method returns the node that is not contained in the specified collection of the failed nodes
      * @param k the key
      * @param notMyVbucketNodes a collection of the nodes are excluded
-     * @return The first MemcachedNode which mets requirements
+     * @return The first MemcachedNode which meets requirements
      */
     public MemcachedNode getAlternative(String k, Collection<MemcachedNode> notMyVbucketNodes) {
         Collection<MemcachedNode> nodes = nodesMap.values();
