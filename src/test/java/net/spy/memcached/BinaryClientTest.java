bc892d226629addabb179acd59d08363a46671aa
Mike Wiederhold <mikewied@comcast.net>
2011-06-15 16:46:02 -0700

    Fixed broken get and touch test
    I was testing these throuh eclipse before and I didn't have
    assertions turned on so I falsely verifed the last commit that I
    checked in
    
    Change-Id: I01ca86771041b13e63f5073193eb3c49ed2947c9
    Reviewed-on: http://review.couchbase.org/7009
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- src/test/java/net/spy/memcached/BinaryClientTest.java
+++ src/test/java/net/spy/memcached/BinaryClientTest.java
@@ -66,12 +66,11 @@ public class BinaryClientTest extends ProtocolBaseCase {
 	public void testGATTimeout() throws Exception {
 		if (isMembase()) {
 			assertNull(client.get("gatkey"));
-			assert client.set("gatkey", 2, "gatvalue").get().booleanValue();
-			Thread.sleep(1000);
-			assert client.getAndTouch("gatkey", 3).equals("gatvalue");
-			Thread.sleep(2000);
+			assert client.set("gatkey", 1, "gatvalue").get().booleanValue();
+			assert client.getAndTouch("gatkey", 2).getValue().equals("gatvalue");
+			Thread.sleep(1300);
 			assert client.get("gatkey").equals("gatvalue");
-			Thread.sleep(1100);
+			Thread.sleep(2000);
 			assertNull(client.getAndTouch("gatkey", 3));
 		}
 	}
