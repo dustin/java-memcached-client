e93aef248894e3f6229220eaa41da89f9aff1890
Mike Wiederhold <mikewied@comcast.net>
2011-06-15 17:20:53 -0700

    Added unit tests for touch
    Added a unit test for touch. Due to dependency issues the unit
    tests for the touch operation could not be checked in with the
    code for the operation.
    
    Change-Id: Icbffd0be4419fe57a3d4cd8fef879be810a79c30
    Reviewed-on: http://review.couchbase.org/6906
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- src/test/java/net/spy/memcached/BinaryClientTest.java
+++ src/test/java/net/spy/memcached/BinaryClientTest.java
@@ -75,4 +75,16 @@ public class BinaryClientTest extends ProtocolBaseCase {
 			assertNull(client.getAndTouch("gatkey", 3));
 		}
 	}
+
+	public void testTouchTimeout() throws Exception {
+		if (isMembase()) {
+			assertNull(client.get("touchkey"));
+			assert client.set("touchkey", 1, "touchvalue").get().booleanValue();
+			assert client.touch("touchkey", 2).get().booleanValue();
+			Thread.sleep(1300);
+			assert client.get("touchkey").equals("touchvalue");
+			Thread.sleep(2000);
+			assertFalse(client.touch("touchkey", 3).get().booleanValue());
+		}
+	}
 }
