0a2fa7c69d598006c932d5f9717ef27a68da0f04
Mike Wiederhold <mikewied@comcast.net>
2011-06-28 15:27:21 -0700

    Added constructor to MemcachedClient that takes a ConnectionFactory
    Previously users of the vBucket Aware MemcachedClient of smart client
    were unable to specify specific values for how connections are
    createded. This commit allows uses to pass a ConnectionFactory into
    a vBucket Aware constructor in the MemcachedClient object.
    
    Change-Id: I01653385c1d125ad868e9e018db311597b6ce725
    Reviewed-on: http://review.couchbase.org/7626
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- src/main/java/net/spy/memcached/ConnectionFactoryBuilder.java
+++ src/main/java/net/spy/memcached/ConnectionFactoryBuilder.java
@@ -50,6 +50,27 @@ public class ConnectionFactoryBuilder {
 	/**
 	 * Set the operation queue factory.
 	 */
+
+    public ConnectionFactoryBuilder() {
+        // empty
+    }
+
+    public ConnectionFactoryBuilder(ConnectionFactory cf) {
+		setAuthDescriptor(cf.getAuthDescriptor());
+		setDaemon(cf.isDaemon());
+		setFailureMode(cf.getFailureMode());
+		setHashAlg(cf.getHashAlg());
+		setInitialObservers(cf.getInitialObservers());
+		setMaxReconnectDelay(cf.getMaxReconnectDelay());
+		setOpQueueMaxBlockTime(cf.getOpQueueMaxBlockTime());
+		setOpTimeout(cf.getOperationTimeout());
+		setReadBufferSize(cf.getReadBufSize());
+		setShouldOptimize(cf.shouldOptimize());
+		setTimeoutExceptionThreshold(cf.getTimeoutExceptionThreshold());
+		setTranscoder(cf.getDefaultTranscoder());
+		setUseNagleAlgorithm(cf.useNagleAlgorithm());
+    }
+
 	public ConnectionFactoryBuilder setOpQueueFactory(OperationQueueFactory q) {
 		opQueueFactory = q;
 		return this;
--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -211,9 +211,41 @@ public class MemcachedClient extends SpyThread
 	 * @throws ConfigurationException if the configuration provided by the
 	 *         server has issues or is not compatible
 	 */
-	public MemcachedClient(final List<URI> baseList,
-		final String bucketName,
+	public MemcachedClient(final List<URI> baseList, final String bucketName,
 		final String usr, final String pwd) throws IOException, ConfigurationException {
+		this(new BinaryConnectionFactory(), baseList, bucketName, usr, pwd);
+	}
+
+	/**
+	 * Get a MemcachedClient based on the REST response from a Membase server
+	 * where the username is different than the bucket name.
+	 *
+	 * Note that when specifying a ConnectionFactory you must specify a
+	 * BinaryConnectionFactory. Also the ConnectionFactory's protocol
+	 * and locator values are always overwritten. The protocol will always
+	 * be binary and the locator will be chosen based on the bucket type you
+	 * are connecting to.
+	 *
+	 * To connect to the "default" special bucket for a given cluster, use an
+	 * empty string as the password.
+	 *
+	 * If a password has not been assigned to the bucket, it is typically an
+	 * empty string.
+	 *
+	 * @param cf the ConnectionFactory to use to create connections
+	 * @param baseList the URI list of one or more servers from the cluster
+	 * @param bucketName the bucket name in the cluster you wish to use
+	 * @param usr the username for the bucket; this nearly always be the same
+	 *        as the bucket name
+	 * @param pwd the password for the bucket
+	 * @throws IOException if connections could not be made
+	 * @throws ConfigurationException if the configuration provided by the
+	 *         server has issues or is not compatible
+	 */
+	public MemcachedClient(ConnectionFactory cf, final List<URI> baseList,
+			final String bucketName, final String usr, final String pwd)
+			throws IOException, ConfigurationException {
+		ConnectionFactoryBuilder cfb = new ConnectionFactoryBuilder(cf);
 		for (URI bu : baseList) {
 			if (!bu.isAbsolute()) {
 				throw new IllegalArgumentException("The base URI must be absolute");
@@ -222,7 +254,12 @@ public class MemcachedClient extends SpyThread
 		this.configurationProvider = new ConfigurationProviderHTTP(baseList, usr, pwd);
 		Bucket bucket = this.configurationProvider.getBucketConfiguration(bucketName);
 		Config config = bucket.getConfig();
-		ConnectionFactoryBuilder cfb = new ConnectionFactoryBuilder();
+
+		if (cf != null && !(cf instanceof BinaryConnectionFactory)) {
+			throw new IllegalArgumentException("ConnectionFactory must be of type " +
+					"BinaryConnectionFactory");
+		}
+
 		if (config.getConfigType() == ConfigType.MEMBASE) {
 			cfb.setFailureMode(FailureMode.Retry)
 				.setProtocol(ConnectionFactoryBuilder.Protocol.BINARY)
@@ -237,12 +274,15 @@ public class MemcachedClient extends SpyThread
 		} else {
 			throw new ConfigurationException("Bucket type not supported or JSON response unexpected");
 		}
+
 		if (!this.configurationProvider.getAnonymousAuthBucket().equals(bucketName) && usr != null) {
 			AuthDescriptor ad = new AuthDescriptor(new String[]{"PLAIN"},
 				new PlainCallbackHandler(usr, pwd));
 			cfb.setAuthDescriptor(ad);
 		}
-		ConnectionFactory cf = cfb.build();
+
+		cf = cfb.build();
+
 		List<InetSocketAddress> addrs = AddrUtil.getAddresses(bucket.getConfig().getServers());
 		if(cf == null) {
 			throw new NullPointerException("Connection factory required");
