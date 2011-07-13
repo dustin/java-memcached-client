a276efab24dcd0d84b2411e0fabceb0f4dddc485
Matt Ingenthron <ingenthr@cep.net>
2011-07-12 20:02:06 -0700

    Also check for RETRY during clone.
    Change-Id: I23953356c77e72e0fa3ce0a1745dee7eb8faba3f
    Reviewed-on: http://review.couchbase.org/8194
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Matt Ingenthron <matt@couchbase.com>

--- src/main/java/net/spy/memcached/ops/BaseOperationFactory.java
+++ src/main/java/net/spy/memcached/ops/BaseOperationFactory.java
@@ -21,7 +21,7 @@ public abstract class BaseOperationFactory implements OperationFactory {
 	}
 
 	public Collection<Operation> clone(KeyedOperation op) {
-		assert op.getState() == OperationState.WRITING
+		assert (op.getState() == OperationState.WRITING || op.getState() == OperationState.RETRY)
 			: "Who passed me an operation in the " + op.getState() + "state?";
 		assert !op.isCancelled() : "Attempted to clone a canceled op";
 		assert !op.hasErrored() : "Attempted to clone an errored op";
