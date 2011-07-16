3d6b55461e6bfd1b92698e6c0a59f361b220589a
Mike Wiederhold <mikewied@comcast.net>
2011-07-16 15:41:24 -0700

    Fixed issue with flags not being added properly to tap messages
    Change-Id: I17156e804155809cab54569c0906c64afeea99c9
    Reviewed-on: http://review.couchbase.org/8074
    Tested-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>

--- src/main/java/net/spy/memcached/tapmessage/TapFlag.java
+++ src/main/java/net/spy/memcached/tapmessage/TapFlag.java
@@ -63,9 +63,9 @@ public enum TapFlag {
 	 * @return Returns true if the flag is contained in the flag field
 	 */
 	boolean hasFlag(int f) {
-		if ((f & (int) flag) == 1) {
-			return false;
+		if ((f & (int) flag) > 0) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 }
