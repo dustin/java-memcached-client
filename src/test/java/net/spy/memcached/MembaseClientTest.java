83f708a3107175f9b15bc78aa3000c084a7c3706
Mike Wiederhold <mikewied@comcast.net>
2011-09-07 12:38:59 -0700

    SPY-26: Add Getl Tests
    Change-Id: I3d512837f31d5e4d46096235a49f65245895de77
    Reviewed-on: http://review.couchbase.org/9452
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- src/test/java/net/spy/memcached/MembaseClientTest.java
+++ src/test/java/net/spy/memcached/MembaseClientTest.java
@@ -67,6 +67,18 @@ public class MembaseClientTest extends BinaryClientTest {
     }
   }
 
+  public void testSimpleGetl() throws Exception {
+    assertNull(client.get("getltest"));
+    client.set("getltest", 0, "value");
+    ((MembaseClient)client).getAndLock("getltest", 3);
+    Thread.sleep(2000);
+    assert !client.set("getltest", 1, "newvalue").get().booleanValue()
+      : "Key wasn't locked for the right amount of time";
+    Thread.sleep(2000);
+    assert client.set("getltest", 1, "newvalue").get().booleanValue()
+      : "Key was locked for too long";
+  }
+
   protected void syncGetTimeoutsInitClient() throws Exception {
     initClient(new MembaseConnectionFactory(Arrays.asList(URI
         .create("http://localhost:8091/pools")), "default", "default", "") {
