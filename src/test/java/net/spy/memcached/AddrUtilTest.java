d49511988e2b37df12fc16a37ab9f3c20a4fa90a
Mike Wiederhold <mikewied@comcast.net>
2011-09-14 14:01:09 -0700

    Fixed test failure issue in testIPv6Host() in AddrUtilTest
    And ipv6 address can return a hostname of 0:0:0:0:0:0:0:1 so
    it needs to be added to the list of valid hostnames that can
    be returned.
    
    Change-Id: I13830b22f58bfa132891a2111e5b4675a297ec51
    Reviewed-on: http://review.couchbase.org/9591
    Tested-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Dustin Sallings <dustin@spy.net>
    Reviewed-by: Dustin Sallings <dustin@spy.net>

--- src/test/java/net/spy/memcached/AddrUtilTest.java
+++ src/test/java/net/spy/memcached/AddrUtilTest.java
@@ -120,6 +120,7 @@ public class AddrUtilTest extends TestCase {
     Set<String> validLocalhostNames = new HashSet<String>();
     validLocalhostNames.add("localhost");
     validLocalhostNames.add("ip6-localhost");
+    validLocalhostNames.add("0:0:0:0:0:0:0:1");
     validLocalhostNames.add("localhost6.localdomain6");
     assert (validLocalhostNames.contains(addrs.get(0).getHostName()));
     assertEquals(80, addrs.get(0).getPort());
