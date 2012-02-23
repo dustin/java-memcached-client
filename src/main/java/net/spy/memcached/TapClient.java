1a4cbeaf5ecadfb26db49ce97a6c1d33b9be94c6
Matt Ingenthron <ingenthr@cep.net>
2012-02-22 21:47:56 -0800

    Wait longer on tap data received.
    Change-Id: I5279eec0f642688013c9a8d98ca4a7922cae9d42
    Reviewed-on: http://review.couchbase.org/13509
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Raghavan N. Srinivas <raghavan.srinivas@gmail.com>
    Tested-by: Matt Ingenthron <matt@couchbase.com>

--- src/main/java/net/spy/memcached/TapClient.java
+++ src/main/java/net/spy/memcached/TapClient.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009-2011 Couchbase, Inc.
+ * Copyright (C) 2009-2012 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -91,7 +91,7 @@ public class TapClient {
    *         empty for more than one second.
    */
   public ResponseMessage getNextMessage() {
-    return getNextMessage(1, TimeUnit.SECONDS);
+    return getNextMessage(10, TimeUnit.SECONDS);
   }
 
   /**
