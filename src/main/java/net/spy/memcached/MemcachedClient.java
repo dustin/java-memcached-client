f6f76efad6fdeaea9e67939036c58562e0a035f6
Michael Nitschinger <michael@nitschinger.at>
2013-10-07 15:58:09 +0200

    SPY-137: Properly shutdown executorService on shutdown().
    Change-Id: I23bf0a30cc96c5721d6ed0d5c16bdfc2113c0fe4
    Reviewed-on: http://review.couchbase.org/29372
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -2209,6 +2209,13 @@ public class MemcachedClient extends SpyObject implements MemcachedClientIF,
     String baseName = mconn.getName();
     mconn.setName(baseName + " - SHUTTING DOWN");
     boolean rv = true;
+    if (connFactory.isDefaultExecutorService()) {
+      try {
+        executorService.shutdown();
+      } catch (Exception ex) {
+        getLogger().warn("Failed shutting down the ExecutorService: ", ex);
+      }
+    }
     try {
       // Conditionally wait
       if (timeout > 0) {
