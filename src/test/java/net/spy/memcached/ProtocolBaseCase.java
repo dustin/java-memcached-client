95342fa4489ecb58f8f357aedf4a767de4fa4ce4
Michael Nitschinger <michael@nitschinger.at>
2014-01-08 18:22:44 +0100

    Add missing import.
    Change-Id: I261bcf045264b041bdea8ec0df0fab3c711cd64f
    Reviewed-on: http://review.couchbase.org/31732
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/net/spy/memcached/ProtocolBaseCase.java
+++ src/test/java/net/spy/memcached/ProtocolBaseCase.java
@@ -30,6 +30,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.Callable;
