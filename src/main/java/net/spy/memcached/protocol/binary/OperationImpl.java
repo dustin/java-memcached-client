ca3c8d4c1d69608bdb7b8b93c0b6a95d3361b947
dominictootell <dominic.tootell@gmail.com>
2014-05-25 14:21:42 +0100

    SPY-175: Reduce memory use in binary GET
    Change-Id: I9b90aab0b54a2ae7d2c262d499ff47482b9d6c6c
    Reviewed-on: http://review.couchbase.org/38359
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/net/spy/memcached/protocol/binary/OperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/binary/OperationImpl.java
@@ -222,37 +222,38 @@ public  abstract class OperationImpl extends BaseOperationImpl
    */
   protected OperationStatus getStatusForErrorCode(int errCode, byte[] errPl)
     throws IOException {
-    errorMsg = new byte[errPl.length];
-    errorMsg = errPl.clone();
 
-    StatusCode statusCode = StatusCode.fromBinaryCode(errCode);
+    if(errCode == SUCCESS) {
+        return STATUS_OK;
+    } else {
+        StatusCode statusCode = StatusCode.fromBinaryCode(errCode);
+        errorMsg = errPl.clone();
 
-    switch (errCode) {
-    case SUCCESS:
-      return STATUS_OK;
-    case ERR_NOT_FOUND:
-      return new CASOperationStatus(false, new String(errPl),
-          CASResponse.NOT_FOUND, statusCode);
-    case ERR_EXISTS:
-      return new CASOperationStatus(false, new String(errPl),
-          CASResponse.EXISTS, statusCode);
-    case ERR_NOT_STORED:
-      return new CASOperationStatus(false, new String(errPl),
-          CASResponse.NOT_FOUND, statusCode);
-    case ERR_2BIG:
-    case ERR_INTERNAL:
-      handleError(OperationErrorType.SERVER, new String(errPl));
-    case ERR_INVAL:
-    case ERR_DELTA_BADVAL:
-    case ERR_NOT_MY_VBUCKET:
-    case ERR_UNKNOWN_COMMAND:
-    case ERR_NO_MEM:
-    case ERR_NOT_SUPPORTED:
-    case ERR_BUSY:
-    case ERR_TEMP_FAIL:
-      return new OperationStatus(false, new String(errPl), statusCode);
-    default:
-      return null;
+        switch (errCode) {
+            case ERR_NOT_FOUND:
+                return new CASOperationStatus(false, new String(errPl),
+                        CASResponse.NOT_FOUND, statusCode);
+            case ERR_EXISTS:
+                return new CASOperationStatus(false, new String(errPl),
+                        CASResponse.EXISTS, statusCode);
+            case ERR_NOT_STORED:
+                return new CASOperationStatus(false, new String(errPl),
+                        CASResponse.NOT_FOUND, statusCode);
+            case ERR_2BIG:
+            case ERR_INTERNAL:
+                handleError(OperationErrorType.SERVER, new String(errPl));
+            case ERR_INVAL:
+            case ERR_DELTA_BADVAL:
+            case ERR_NOT_MY_VBUCKET:
+            case ERR_UNKNOWN_COMMAND:
+            case ERR_NO_MEM:
+            case ERR_NOT_SUPPORTED:
+            case ERR_BUSY:
+            case ERR_TEMP_FAIL:
+                return new OperationStatus(false, new String(errPl), statusCode);
+            default:
+                return null;
+        }
     }
   }
 
