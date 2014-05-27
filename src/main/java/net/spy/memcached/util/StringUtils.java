de62e2b15464f82e952bcb9d2be006075cec7f3b
Michael Nitschinger <michael@nitschinger.at>
2014-05-27 10:59:49 +0200

    SPY-170: Fix concurrency issue in StringUtils.isJSONObject()
    Motivation
    ----------
    A concurrency issue was reported in the StringUtils class, pointing towards
    the pattern matching of the JSON string.
    
    Modifications
    -------------
    It turned out that the matcher is not thread safe and there is a race condition
    between clearing it and then matching on it. By falling back to the thread-safe
    builder directly the race condition goes away.
    
    Result
    ------
    No concurrency issue anymore in the helper method.
    
    Change-Id: I09729af78bd241ff8dcb0869992d3ff474fb6ec1
    Reviewed-on: http://review.couchbase.org/37603
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/net/spy/memcached/util/StringUtils.java
+++ src/main/java/net/spy/memcached/util/StringUtils.java
@@ -27,7 +27,6 @@ import net.spy.memcached.MemcachedClientIF;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -41,11 +40,6 @@ public final class StringUtils {
   private static final Pattern decimalPattern = Pattern.compile("^-?\\d+$");
 
   /**
-   * The matcher for the decimal pattern regex.
-   */
-  private static final Matcher decimalMatcher = decimalPattern.matcher("");
-
-  /**
    * Maximum supported key length.
    */
   private static final int MAX_KEY_LENGTH = MemcachedClientIF.MAX_KEY_LENGTH;
@@ -55,7 +49,7 @@ public final class StringUtils {
    */
   private static final IllegalArgumentException KEY_TOO_LONG_EXCEPTION =
     new IllegalArgumentException("Key is too long (maxlen = "
-      + MAX_KEY_LENGTH + ")");
+      + MAX_KEY_LENGTH + ')');
 
   /**
    * Exception thrown if the input key is empty.
@@ -112,7 +106,7 @@ public final class StringUtils {
 
     if (s.startsWith("{") || s.startsWith("[")
       || "true".equals(s) || "false".equals(s)
-      || "null".equals(s) || decimalMatcher.reset(s).matches()) {
+      || "null".equals(s) || decimalPattern.matcher(s).matches()) {
       return true;
     }
 
