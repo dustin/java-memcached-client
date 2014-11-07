8af3ebbcc2cd74deee6e28350322dbc470f5164f
Michael Nitschinger <michael@nitschinger.at>
2014-11-07 11:27:58 +0100

    SPY-179: Reconnect has wrong ceiling, leading to very long reconnect delays.
    Motivation
    ----------
    This is a regression which was introduced through the change in SPY-158
    (https://github.com/couchbase/spymemcached/commit/bfaec2b4f1385f20f32fd0ed0736b52ef572e340).
    
    Because the time is now converted to millis right away, but the comparison checks with the 2^n
    still converted to seconds. That way, even with a very high N it is smaller than the default of
    30k.
    
    Modifications
    -------------
    The code now correctly converts the 2^n to milliseconds before comparing it, making it a comparison
    on the same unit of time again.
    
    Result
    ------
    The ceiling is now properly respected and as a result, reconnect delays are properly capped at
    their defined maximum.
    
    Change-Id: I0c976344d0f8e09bc041d94f3544620ef4fb3117
    Reviewed-on: http://review.couchbase.org/42925
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -977,7 +977,7 @@ public class MemcachedConnection extends SpyThread {
     node.setChannel(null);
 
     long delay = (long) Math.min(maxDelay, Math.pow(2,
-        node.getReconnectCount())) * 1000;
+        node.getReconnectCount()) * 1000);
     long reconnectTime = System.currentTimeMillis() + delay;
     while (reconnectQueue.containsKey(reconnectTime)) {
       reconnectTime++;
