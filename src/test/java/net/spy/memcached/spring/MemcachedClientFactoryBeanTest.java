4bb435b7b58b78d650aad5019cd5c1569359257b
Michael Nitschinger <michael@nitschinger.at>
2015-01-19 10:08:28 +0100

    Fix spring unit tests.
    Motivation
    ----------
    afterPropertiesSet() needs to be called on the factory to actually
    create the client so that it does not result in a NPE.
    
    Change-Id: I483c045451f90ff3ed01d00db7e912e7d2484b64
    Reviewed-on: http://review.couchbase.org/45554
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/test/java/net/spy/memcached/spring/MemcachedClientFactoryBeanTest.java
+++ src/test/java/net/spy/memcached/spring/MemcachedClientFactoryBeanTest.java
@@ -55,6 +55,8 @@ public class MemcachedClientFactoryBeanTest extends TestCase {
     final Transcoder<Object> transcoder = new SerializingTranscoder();
     factory.setTranscoder(transcoder);
 
+    factory.afterPropertiesSet();
+
     final MemcachedClient memcachedClient =
         (MemcachedClient) factory.getObject();
 
